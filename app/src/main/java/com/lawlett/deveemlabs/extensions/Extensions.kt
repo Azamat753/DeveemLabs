9122e60743c656290f3821997883efa854ed220b
azamat753 <azamat.nazar99@gmail.com>
2020-12-31 19:00:31 +0600

    -logo

--- app/src/main/java/com/lawlett/deveemlabs/extensions/Extensions.kt
+++ app/src/main/java/com/lawlett/deveemlabs/extensions/Extensions.kt
@@ -1,20 +1,8 @@
 package com.lawlett.deveemlabs.extensions
 
-import android.view.View
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
 
 fun ImageView.loadImage(url: String) {
     Picasso.get().load(url).into(this)
-}
-fun View.visible() {
-    this.visibility = View.VISIBLE
-}
-
-fun View.gone() {
-    this.visibility = View.GONE
-}
-
-fun View.invisible() {
-    this.visibility = View.INVISIBLE
 }
--- app/src/main/java/com/lawlett/deveemlabs/main/MainActivity.kt
+++ app/src/main/java/com/lawlett/deveemlabs/main/MainActivity.kt
@@ -2,14 +2,8 @@ package com.lawlett.deveemlabs.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import com.lawlett.deveemlabs.R
 import com.lawlett.deveemlabs.adapter.PagerAdapter
-import com.lawlett.deveemlabs.extensions.gone
-import com.lawlett.deveemlabs.extensions.visible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
